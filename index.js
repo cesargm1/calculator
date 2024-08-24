cf388d2fa18f7979af05f24d40caf46512cd7f8c
cesargm <cg.gtav@gmail.com>
2024-08-24 20:25:20 +0200

    calculator

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
-function putDigit(digit){
-    const result = document.getElementById('result')
+const result = document.getElementById('result')
+const calculator = document.querySelector(".calculadora")
+const butons = document.querySelectorAll("button")
+let acc = 0
+let operator = null
+result.value = 0
 
-    result.value= result.value+digit
+
+const putDigit = (number) => {
+    result.value = parseFloat(result.value + number)
+
 }
 
-function reset(){
-    const result = document.getElementById ('result')
-    result.value=''
+const reset = () => {
+     result.value = 0
 }
+
+const add = () => {
+     acc = result.value
+     operator = '+'
+     result.value = 0
+}
+
+const less = () => {
+     acc = result.value
+     operator = '-'
+     result.value = 0
+}
+
+
+const division  =  () => {
+     acc = result.value
+     operator = ':'
+     result.value = 0
+}
+
+const  multipli  =  () => {
+     acc = result.value
+     operator = 'x'
+     result.value = 0
+}
+
+const  potencia  =  () => {
+     acc = result.value
+     operator = 'potencia'
+     result.value = 0
+}
+
+const equal = () => {
+    if (operator === '+' ) {
+     result.value = parseFloat(acc) + parseFloat(result.value)
+    } 
+    if (operator === '-' ) {
+     result.value = parseFloat(acc) - parseFloat(result.value)
+    } 
+
+    if (operator === 'x' ) {
+     result.value = parseFloat(acc) * parseFloat(result.value)
+    } 
+
+    if (operator === ':' ) {
+     result.value = parseFloat(acc) / parseFloat(result.value)
+    } 
+
+    if (operator === 'potencia' ) {
+     result.value = parseFloat(acc) ** parseFloat(result.value)
+    } 
+}
+
+// keyboardNum.addEventListener("click",putScreen)
+
